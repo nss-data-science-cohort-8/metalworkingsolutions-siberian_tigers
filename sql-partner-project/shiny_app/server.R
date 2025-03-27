0ea553e216918d2ad16ca810e641c14a2ee66f5a
Mythical-clj <cavinjacobson@gmail.com>
2025-03-27 15:07:52 -0500

    App finished. 3/27

--- sql-partner-project/shiny_app/server.R
+++ sql-partner-project/shiny_app/server.R
@@ -2,29 +2,14 @@
 
 function(input, output, session) {
   
-  # updateSelectizeInput(
-  #   session, 
-  #   'Chosen_Firm', 
-  #   choices = (Firm_Industry_Random_Effect_Coeff_Desc_StatSig_y_StatInsig$level), 
-  #   selected = 'Costco Wholesale Corp', 
-  #   server = TRUE)
-  
-  # plot_data_func <- reactive({
-  #   
-  #   plot_data <- LeftJoined_YMDseries_TopNParts
-  #   
-  #   return(plot_data)    
-  #   
-  # })
-  
+  #Q2b
   
   output$distPlot_Q2b <- renderPlotly({
     
     title <- glue("Trend in Quantity Ordered for Most High Volume Parts")
     
-    # plot_data_func() |>  
     
-      TopVol_Parts <- ggplot(LeftJoined_YMDseries_TopNParts, aes(x=Production_Due_Date_DateTimeFormat, y=total_quantity_for_manufactured_part)) +
+    TopVol_Parts <- ggplot(LeftJoined_YMDseries_TopNParts, aes(x=Production_Due_Date_DateTimeFormat, y=total_quantity_for_manufactured_part)) +
       geom_line(data = LeftJoined_YMDseries_TopNParts |> dplyr::select(-PartID_LongDescription), aes(group=PartID_LongDescription2), color="grey", linewidth=0.5, alpha=0.5) +
       geom_point( aes(color=PartID_LongDescription), color="#69b3a2" ) +
       labs(title = 'Seasonal Trend in Quantity Ordered', 
@@ -58,29 +43,14 @@ function(input, output, session) {
   #Q2a
   
   
-  output$parts_table <- renderDataTable(datatable(complete_data |> 
-                                                    group_by(part_id, part_name) |> 
-                                                    summarize(jobs = max(number_of_jobs_by_part),
-                                                              `Estimated Hours` = round(sum(estimated_production_hours), 2),
-                                                              Revenue = comma(sum(total_revenue), accuracy = 0.01),
-                                                              `Estimated Hours Per Job` = round((sum(estimated_production_hours) / min(number_of_jobs_by_part)), 2), 
-                                                              `Estimated Revenue per Job` = round(sum(total_revenue) / min(number_of_jobs_by_part), 2),
-                                                              `Revenue per Hour` = round(mean(estimated_revenue_per_hour)), 2) |> 
-                                                    rename(`Part ID` = part_id, `Part Name` = part_name) |>
-                                                    select(-`2`) |> 
-                                                    arrange(desc(jobs)), options = list(displayStart = (input$graph_slide[1] - 1))) 
+  output$parts_table <- renderDataTable(datatable(parts_table |> 
+                                                    arrange(desc(jobs)), options = list(displayStart = (input$graph_slide[1] - 1)
+                                                    )
+  ) 
   )
   
   output$distPlot_Q2a <- renderPlot({
-    parts_table_top_10 <- complete_data |> 
-      group_by(part_id) |>
-      summarize(jobs = min(number_of_jobs_by_part),
-                estimated_hours = sum(estimated_production_hours),
-                revenue = sum(total_revenue),
-                estimated_hours_per_job = (sum(estimated_production_hours) / min(number_of_jobs_by_part)), 
-                estimated_revenue_per_job = sum(total_revenue) / min(number_of_jobs_by_part),
-                revenue_per_hour = mean(estimated_revenue_per_hour)) |>
-      arrange(desc(jobs))
+    parts_table_top_10
     
     sf <- max(parts_table_top_10[input$graph_slide[1]:(input$graph_slide[1] + 9), ]$jobs)/max(parts_table_top_10[input$graph_slide[1]:(input$graph_slide[1] + 9), ]$estimated_hours)
     
@@ -117,171 +87,148 @@ function(input, output, session) {
   
   
   
+  #Q2d  
   
   
-  
-  
-  
-output$rev_scatter <- renderPlotly({
-  
-  rev_plot <- ggplot(revenue_data_plot, 
-                     aes(
-                       x = total_quantity_shipped, 
-                       y = estimated_revenue_per_hour, 
-                       size = total_revenue, 
-                       color = part_name,
-                       text = rev_tooltip)) + 
-    geom_point(
-      alpha = 0.5,
-      show.legend = TRUE
+  output$rev_scatter <- renderPlotly({
+    
+    rev_plot <- ggplot(revenue_data_plot, 
+                       aes(
+                         x = total_quantity_shipped, 
+                         y = estimated_revenue_per_hour, 
+                         size = total_revenue, 
+                         color = part_name,
+                         text = rev_tooltip
+                       )
     ) + 
-    labs(title = 'Revenue per Estimated Production Hours by Volume Shipped (> 1,000 Units)',
-         x = 'Total Quantity Shipped (Log Scale)',
-         y = 'Revenue per Est Production Hr (USD/Hr)'
-    ) +
-    scale_x_log10(
-      breaks = c(1000, 2500, 5000, 10000, 15000, 30000, 60000, 100000),
-      labels = comma
-    ) +
-    scale_y_continuous(labels = comma) + 
-    theme(legend.position = "none")
+      geom_point(
+        alpha = 0.5,
+        show.legend = TRUE,
+      ) + 
+      scale_color_manual(values = repeated_colors) +
+      labs(title = 'Revenue per Estimated Production Hours by Volume Shipped (> 1,000 Units)',
+           x = 'Total Quantity Shipped (Log Scale)',
+           y = 'Revenue per Est Production Hr (USD/Hr)'
+      ) +
+      scale_x_log10(
+        breaks = c(1000, 2500, 5000, 10000, 15000, 30000, 60000, 100000),
+        labels = comma
+      ) +
+      scale_y_continuous(labels = comma) + 
+      theme(legend.position = "none")
+    
+    ggplotly(rev_plot, tooltip = 'text', height = 600, width = 1200)
+    
+  })
   
-  ggplotly(rev_plot, tooltip = 'text', height = 600, width = 1200)
+  output$rev_table <- renderDT({
+    datatable(revenue_data_table)
+  })
   
-})
-
-output$rev_table <- renderDT({
-  datatable(
-    revenue_data |>
-      mutate(
-        total_quantity_shipped = comma(total_quantity_shipped),  
-        est_prod_hours = comma(est_prod_hours),                 
-        total_revenue = comma(total_revenue, accuracy = 0.01),  
-        estimated_revenue_per_hour = comma(estimated_revenue_per_hour, accuracy = 0.01)
-      ) |> 
-      select(part_id, 
-             part_name,
-             total_quantity_shipped, 
-             est_prod_hours, 
-             total_revenue, 
-             estimated_revenue_per_hour, 
-             number_of_jobs),
-    colnames = c(
-      'Part Id',
-      'Part Name', 
-      'Quantity Shipped', 
-      'Total Est Prod Hours', 
-      'Total Revenue ($)', 
-      'Revenue per Est Prod Hour ($)', 
-      'Total Jobs'
-    ),
-    options = list(
-      order = list(list(6, "desc")) 
-    )
-  )
-})
-
-#Q2c
-custom_theme = theme(
-  title = element_text(size = 20),
-  axis.title.x = element_text(size = 18),
-  axis.text.x = element_text(size = 16),
-  axis.title.y = element_text(size = 18))
   
-
-output$opsTable <- DT::renderDT({
-  plot_data <- hours_ops
   
-  # if (input$process != "ALL"){
-  #   
-  #   plot_data <- hours_ops |> 
-  #     filter(short_description == input$process)
-  # }
-  plot_data |> 
-    DT::datatable(
-      colnames = c("Process" = "short_description", 
-                   "Number of Jobs" = "num_jobs", 
-                   "Total Hours" = "total_hours", 
-                   "Average Hours" = "avg_hr",
-                   "Average Difference between Estimated and Actual Hours" = 'avg_hr_diff')
-    )
-})
-
-output$opsChart <- renderPlot ({
+  #Q2c
   
-  title <- "Summary of Processes"
   
-  plot_data <- hours_ops_longer
+  custom_theme = theme(
+    title = element_text(size = 20),
+    axis.title.x = element_text(size = 18),
+    axis.text.x = element_text(size = 16),
+    axis.title.y = element_text(size = 18))
   
-  # if (input$process != "ALL"){
-  #   
-  #   plot_data <- hours_ops_longer |> 
-  #     filter(short_description == input$process)
-  # }
-  plot_data |> 
-    ggplot(aes(x=factor(short_description, level=c('ZINC_PLATE', 
-                                                   'POWDER_COAT', 
-                                                   'GALVANIZE', 
-                                                   'WELD', 
-                                                   'PART_TRANSFER',
-                                                   'MACHINE',
-                                                   'TURRET_PUNCH',
-                                                   'OTHER',
-                                                   'PRESS_BRAKE',
-                                                   'SAW',
-                                                   'LASER',
-                                                   'PACK',
-                                                   'SET_UP')), 
-               y = hours, 
-               fill = measurement)) +
-    geom_col(color="black", alpha=.6, position='dodge') +
-    scale_x_discrete(guide = guide_axis(angle = 90)) +
-    theme(legend.title = element_blank()) +
-    scale_fill_hue(labels = c("Average Total Hours", "Average Difference in Hours (Est v Act)")) +
-    scale_fill_manual(values = c("#69b3a2", "grey")) +
-    labs(
-      title = title,
-      x = "Process",
-      y = 'Hours'
-    ) +
-    custom_theme
-})
-
-output$partsTable <- DT::renderDT({
-  hours_parts |>
-    DT::datatable(
-      colnames = c("Part ID" = "part_id", 
-                   "Number of Parts" = "num_parts", 
-                   "Total Hours" = "total_hours", 
-                   "Average Hours" = "avg_hr",
-                   "Average Difference between Estimated and Actual Hours" = 'avg_hr_diff')
-    )
-})
-
-output$partsChart <- renderPlot ({
   
-  title <- "Avg Difference between Estimated and Actual Production Hours"
+  output$opsTable <- DT::renderDT({
+    plot_data <- hours_ops
+    
+    # if (input$process != "ALL"){
+    #   
+    #   plot_data <- hours_ops |> 
+    #     filter(short_description == input$process)
+    # }
+    plot_data |> 
+      DT::datatable(
+        colnames = c("Process" = "short_description", 
+                     "Number of Jobs" = "num_jobs", 
+                     "Total Hours" = "total_hours", 
+                     "Average Hours" = "avg_hr",
+                     "Average Difference between Estimated and Actual Hours" = 'avg_hr_diff')
+      )
+  })
   
-  hours_parts |> 
-    ggplot(aes(x=factor(part_id, level=c('Y002-0562',
-                                         'C057-0000I',
-                                         'U013-0001',
-                                         'S046-0169',
-                                         'F022-0007',
-                                         'S046-0156',
-                                         'M030-0004')), 
-               y=avg_hr_diff)) +
-    geom_col() +
-    scale_x_discrete(guide = guide_axis(angle = 90)) +
-    labs(
-      title = title,
-      x = 'Part ID',
-      y = 'Difference in Hours'
-    ) +
-    custom_theme
+  output$opsChart <- renderPlot ({
+    
+    title <- "Average Production Hours and Difference in Hours (Est. vs. Actual) by Process"
+    
+    plot_data <- hours_ops_longer
+    
+    # if (input$process != "ALL"){
+    #   
+    #   plot_data <- hours_ops_longer |> 
+    #     filter(short_description == input$process)
+    # }
+    plot_data |> 
+      ggplot(aes(x=factor(short_description, level=c('ZINC_PLATE', 
+                                                     'POWDER_COAT', 
+                                                     'GALVANIZE', 
+                                                     'WELD', 
+                                                     'PART_TRANSFER',
+                                                     'MACHINE',
+                                                     'TURRET_PUNCH',
+                                                     'OTHER',
+                                                     'PRESS_BRAKE',
+                                                     'SAW',
+                                                     'LASER',
+                                                     'PACK',
+                                                     'SET_UP')), 
+                 y = hours, 
+                 fill = measurement)) +
+      geom_col(color="black", alpha=.6, position='dodge') +
+      scale_x_discrete(guide = guide_axis(angle = 90)) +
+      theme(legend.title = element_blank()) +
+      scale_fill_manual(values = c("#69b3a2", "grey"), labels = c("Avg Total Hrs", "Avg Hrs Diff")) +
+      labs(
+        title = title,
+        x = "Process",
+        y = 'Hours'
+      ) +
+      custom_theme
+  })
   
-})
-
+  output$partsTable <- DT::renderDT({
+    hours_parts |>
+      DT::datatable(
+        colnames = c("Part ID" = "part_id", 
+                     "Number of Parts" = "num_parts", 
+                     "Total Hours" = "total_hours", 
+                     "Average Hours" = "avg_hr",
+                     "Average Difference between Estimated and Actual Hours" = 'avg_hr_diff')
+      )
+  })
+  
+  output$partsChart <- renderPlot ({
+    
+    title <- "Average Difference between Estimated and Actual Production Hours"
+    
+    hours_parts |> 
+      ggplot(aes(x=factor(part_id, level=c('Y002-0562',
+                                           'C057-0000I',
+                                           'U013-0001',
+                                           'S046-0169',
+                                           'F022-0007',
+                                           'S046-0156',
+                                           'M030-0004')), 
+                 y=avg_hr_diff)) +
+      geom_col(color="black", fill = "#69b3a2", alpha=.6) +
+      scale_x_discrete(guide = guide_axis(angle = 90)) +
+      labs(
+        title = title,
+        x = 'Part ID',
+        y = 'Difference in Hours'
+      ) +
+      custom_theme
+    
+  })
+  
 }
 
 
--- sql-partner-project/shiny_app/ui.R
+++ sql-partner-project/shiny_app/ui.R
@@ -5,8 +5,10 @@ fluidPage(
   theme = shinytheme("readable"),
   
   tags$style(HTML("
-    .tabbable > .nav > li > a                  {background-color: seashell;  color:blue4}
-    .tabbable > .nav > li[class=active]    > a {background-color: dodgerblue; color:white}
+    .nav-tabs > li > a { color: white; }
+    .tabbable > .nav > li > a                  {background-color: grey}
+    .tabbable > .nav > li[class=active]    > a {background-color: #69b3a2}
+    .js-irs-0 .irs-single, .js-irs-0 .irs-bar-edge, .js-irs-0 .irs-bar {background: #69b3a2}
   ")),
   
   # Application title
@@ -19,64 +21,75 @@ fluidPage(
     # Main Panel's top portion has bar graph; bottom portion has table
     mainPanel(
       tabsetPanel(
-        tabPanel(h4('Total Job Volume vs. Total Estimated Production Hours'),
-                 fluidRow(
-                   column( 
-                     width = 12, 
-                     div(class = "dynamic_height",
-                         style = "display: flex; justify-content: center; align-items: center"),
-                     sliderInput('graph_slide',
-                                 'Select Parts as Arranged by Volume of Associated Jobs',
-                                 min = 1, max = 1000,
-                                 step = 10, value = 1)
-                   )
-                 ),
-                 fluidRow(
-                   column(
-                     width = 9, 
-                     div(class = "dynamic_height",
-                         style = "display: flex; justify-content: left; align-items: left"),
-                     plotOutput("distPlot_Q2a", height = "900px"),
-                   ),
-                   column(
-                     width = 3,
-                     div(class = "dynamic_height",
-                         style = "display: flex; justify-content: right; align-items: right"),
-                     h4(HTML(
-                       'Parts with the highest amount of hours per job:<br/>
+        tabPanel(
+          h4('Total Job Volume vs. Total Estimated Production Hours'),
+          fluidRow(
+            column( 
+              width = 12, 
+              div(class = "dynamic_height; slider-custom",
+                  style = "display: flex; justify-content: center; align-items: center"),
+              sliderInput('graph_slide',
+                          'Select Parts as Arranged by Volume of Associated Jobs',
+                          min = 0, max = 1000,
+                          step = 10, value = 0,
+                          width = '70%')
+            )
+          ),
+          fluidRow(
+            column(
+              width = 9, 
+              div(class = "dynamic_height",
+                  style = "display: flex; justify-content: left; align-items: left"),
+              plotOutput("distPlot_Q2a", height = "900px"),
+            ),
+            column(
+              width = 3,
+              div(class = "dynamic_height",
+                  style = "display: flex; justify-content: right; align-items: right"),
+              h4(HTML(
+                'Parts with the highest amount of hours per job:<br/>
                       1. C-AL KIT FOR KOHLER<br/>
-                      2. MWS-Armored Transport<br/>
+                      2. MWS-Armored Transport 6x8<br/>
                       3. BUY STEEL KIT<br/>
                       4. B-AO & D-AO CUMMINS KIT 84 SQFT<br/>
-                      5. NEMA 12 SKID MNT, ENCLOSURE<br/'
-                     )),
-                     h5(HTML('copy and paste in search bar below for numbers'))
-                   )
-                 ),
-                 fluidRow(
-                   column(
-                     width = 12,
-                     div(class = "dynamic_height",
-                         style = "display: flex; justify-content: center; align-items: center"),
-                     dataTableOutput("parts_table")
-                   )
-                 )
+                      5. NEMA 12 SKID MNT, ENCLOSURE<br/>
+                      6. MWS-Armored Transport 8x8<br/>
+                      7. YT449800B-C-AO CUMMINS KIT 84 SQFT<br/>
+                      8. EN 1978 SKY WHITE PEEL<br/>
+                      9. A49149-81-824 46.125 X 23.938<br/>
+                      10. DRIVER CAGE 2 SIDES RH & LH 8 FT X 6 FT W/ DOCUMENT PASS THROUGH<br/>'
+              )),
+              h4(HTML('It appears that parts that are quite large in nature
+                             (vehicles, large steel kits, etc.) tend to take up the most production hours.
+                             There is a low correlation (0.24) between the amount of jobs and production hours<br/>')),
+              h5(HTML('copy and paste the names above in  the search bar below for numbers'))
+            )
+          ),
+          fluidRow(
+            column(
+              width = 12,
+              div(class = "dynamic_height",
+                  style = "display: flex; justify-content: center; align-items: center"),
+              dataTableOutput("parts_table")
+            )
+          )
         ),
         tabPanel(h4('Seasonal Trend for Most In Demand Parts'),  
                  fluidRow(
                    column( 
-                     width = 9, 
+                     width = 10, 
                      div(class = "dynamic_height",
                          style = "display: flex; justify-content: center; align-items: center"),
                      plotlyOutput("distPlot_Q2b", height = "900px")
                    ),
                    column( 
-                     width = 3, 
+                     width = 2, 
                      div(class = "dynamic_height",
                          style = "display: flex; justify-content: center; align-items: center"),
                      h4(HTML(
-                       'Information about this data'
-                     ))
+                       'Insight: The most in demand parts tend to be household items (e.g. mounting bracket for wall shelves), automotive suspension parts, and house renovation items (e.g. bottom plate for foundational jack). Some seasonal trends can be observed here, e.g. season(s) for building and renovating houses.'
+                     )),
+                     h5('The top six parts are displayed in this chart and are listed in the table below.')
                    )
                  ),
                  fluidRow(
@@ -109,7 +122,7 @@ fluidPage(
                                     div(class = "dynamic_height",
                                         style = "display: flex; justify-content: center; align-items: center"),
                                     h4(HTML(
-                                      'Information about this data'
+                                      'Based on the data, it appears that coating or galvanizing are the jobs that take the most amount of time. Additionally, it appears that most jobs complete on schedule.'
                                     )
                                     )
                                   )
@@ -129,7 +142,7 @@ fluidPage(
                                     div(class = "dynamic_height",
                                         style = "display: flex; justify-content: right; align-items: right"),
                                     h4(HTML(
-                                      'Information about this data'
+                                      'Overall, it appears that most parts are prepared on schedule, with only the highlighted handful deviating from that.'
                                     ))
                                   )
                                 )
@@ -158,7 +171,9 @@ fluidPage(
                                     div(class = "dynamic_height",
                                         style = "display: flex; justify-content: right; align-items: right"),
                                     h4(HTML( 
-                                      "Insight: Parts that generate the largest amount of total revenue tend not to generate large amount of revenue per hour of labor."
+                                      'High volume parts typically do not generate the most revenue per hour compared to estimated production hours.<br><br> 
+                                      Most high-volume parts fall below the average revenue per hour. <br><br>
+                                      Parts that generate the highest total revenue often have lower revenue per hour.' 
                                     )
                                     )
                                   )
